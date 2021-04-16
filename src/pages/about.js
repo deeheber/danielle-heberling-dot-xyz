1ca9fe8e26467eea18fe805067606397d68a2fa9
Danielle Heberling <daheber@gmail.com>
2021-04-16 07:11:45 -0700

    Update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -33,7 +33,7 @@ class About extends Component {
                   Career wise, I used to work in tech support for <a href='https://support.apple.com/' target='_blank' rel='noopener noreferrer'>AppleCare</a> and then <a href='https://www.squarespace.com/' target='_blank' rel='noopener noreferrer'>Squarespace</a>. I loved helping customers; however, I discovered that my job centered around asking an engineer to fix a bug or UX issue.
                 </p>
                 <p>
-                  Because of my desire to provide a better customer experience and to take matters into my own hands, I began teaching myself how to code and became a software engineer. Currently working on a cloud-based connect, develop and deployment solution for cellular and bluetooth low energy IoT devices at <a href='https://nrfcloud.com/#/' target='_blank' rel='noopener noreferrer'>Nordic Semiconductor</a>.
+                  Because of my desire to provide a better customer experience and to take matters into my own hands, I began teaching myself how to code and became a software engineer. Currently interested in Serverless architectures, Javascript/Typescript, and AWS though that will probably evolve over time as new technologies come out.
                 </p>
                 <p>
                   This information on this site serves as a personal reminder of my commitment to life long learning, improvement, creating things, and knowledge sharing.
