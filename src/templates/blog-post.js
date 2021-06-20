408ebbcd938ff7a2738f0bee9edf8619d4ed255d
deeheber <daheber@gmail.com>
2021-06-19 19:09:34 -0700

    Remove class components from blog pages

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,66 +1,64 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link, graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 import Seo from '../components/seo';
 import { rhythm, scale } from '../utils/typography';
 
-class BlogPostTemplate extends Component {
-  render () {
-    const post = this.props.data.markdownRemark;
-    const siteTitle = this.props.data.site.siteMetadata.title;
-    const excerpt = post.excerpt.replace(/Photo by (.*?) on Unsplash/i, '');
-    const { previous, next } = this.props.pageContext;
+const BlogPostTemplate = ({ data, pageContext }) => {
+  const post = data.markdownRemark;
+  const siteTitle = data.site.siteMetadata.title;
+  const excerpt = post.excerpt.replace(/Photo by (.*?) on Unsplash/i, '');
+  const { previous, next } = pageContext;
 
-    return (
-      <Layout title={siteTitle}>
-        <Seo title={post.frontmatter.title} description={excerpt} />
-        <h3>{post.frontmatter.title}</h3>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: 'block',
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1)
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1)
-          }}
-        />
+  return (
+    <Layout title={siteTitle}>
+      <Seo title={post.frontmatter.title} description={excerpt} />
+      <h3>{post.frontmatter.title}</h3>
+      <p
+        style={{
+          ...scale(-1 / 5),
+          display: 'block',
+          marginBottom: rhythm(1),
+          marginTop: rhythm(-1)
+        }}
+      >
+        {post.frontmatter.date}
+      </p>
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      <hr
+        style={{
+          marginBottom: rhythm(1)
+        }}
+      />
 
-        <ul
-          style={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'space-between',
-            listStyle: 'none',
-            padding: 0
-          }}
-        >
-          <li>
-            {previous && (
-              <Link style={{ boxShadow: 'none' }} to={`/blog${previous.fields.slug}`} rel='prev'>
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link style={{ boxShadow: 'none' }} to={`/blog${next.fields.slug}`} rel='next'>
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </Layout>
-    );
-  }
-}
+      <ul
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'space-between',
+          listStyle: 'none',
+          padding: 0
+        }}
+      >
+        <li>
+          {previous && (
+            <Link style={{ boxShadow: 'none' }} to={`/blog${previous.fields.slug}`} rel='prev'>
+              ← {previous.frontmatter.title}
+            </Link>
+          )}
+        </li>
+        <li>
+          {next && (
+            <Link style={{ boxShadow: 'none' }} to={`/blog${next.fields.slug}`} rel='next'>
+              {next.frontmatter.title} →
+            </Link>
+          )}
+        </li>
+      </ul>
+    </Layout>
+  );
+};
 
 export default BlogPostTemplate;
 
