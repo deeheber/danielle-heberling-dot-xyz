c473c564c2f5f330723e9e0c82f05aee4692643d
Danielle Heberling <daheber@gmail.com>
2021-10-10 07:55:00 -0700

    Bio refresh (#50)
    * Archive post
    
    * Update npm deps
    
    * Update about bio
    
    * Moar minor bio tweaks
    
    * Moar moar bio updates

--- src/pages/about.js
+++ src/pages/about.js
@@ -23,19 +23,20 @@ const About = ({ data }) => {
           />
         </div>
         <p>
-          Throughout my entire life, I questioned why and how things work with a strong desire to make them better.
+          Throughout my entire life, I questioned why and how things work with a strong interest in making them better.
         </p>
         <p>
-          Career wise, I used to work in tech support for <a href='https://support.apple.com/' target='_blank' rel='noopener noreferrer'>AppleCare</a> and then <a href='https://www.squarespace.com/' target='_blank' rel='noopener noreferrer'>Squarespace</a>. I loved helping customers; however, I discovered that my job centered around asking an engineer to fix a bug or UX issue.
+          Career wise, I used to work in tech support for <a href='https://support.apple.com/' target='_blank' rel='noopener noreferrer'>AppleCare</a> and later <a href='https://www.squarespace.com/' target='_blank' rel='noopener noreferrer'>Squarespace</a>. I enjoyed helping customers; however, I felt that my job centered around asking an engineer to fix a bug or UX issue.
         </p>
         <p>
-          Because of my desire to provide a better customer experience and to take matters into my own hands, I began teaching myself how to code and became a software engineer.
+          Because of my desire to provide a better customer experience and to take matters into my own hands, I taught myself how to code and became a software engineer.
         </p>
+        <p>Currently loving TypeScript/JavaScript, GraphQL, AWS SAM, and AWS CDK, but I'm open to learning new languages and technologies if it makes sense for the project/team, or matches my interest.</p>
         <p>
           The information on this site serves as a personal reminder of my commitment to life long learning, improvement, creating things, and knowledge sharing. Feel free to reach out anytime.
         </p>
         <p>
-          Since technology is always evolving, disclaimer that older content might contain out of date information and/or examples.
+          Since technology is always evolving, disclaimer that older content might contain out of date information and examples.
         </p>
         <p>
           Any opinions expressed on this site are mine and are not reflective of any organizations that I am affiliated with.
