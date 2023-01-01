3534abc084dc93aac2f7307de6cf57bfbf24a4d0
Danielle Heberling <deeheber@users.noreply.github.com>
2022-12-31 18:31:04 -0800

    Update GQL sort and agg fields

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,9 +80,8 @@ module.exports = {
             },
             query: `
               {
-                allMarkdownRemark(
-                  sort: { order: DESC, fields: [frontmatter___date] },
-                ) {
+                allMarkdownRemark(sort: {frontmatter:
+                  {date: DESC}}) {
                   edges {
                     node {
                       excerpt
