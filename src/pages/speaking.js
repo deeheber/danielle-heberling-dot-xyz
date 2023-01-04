0125c1d0f186c761295d780168de7990d05af9d6
Danielle Heberling <deeheber@users.noreply.github.com>
2023-01-04 11:06:30 -0800

    Unordered list for speaking page

--- src/pages/speaking.js
+++ src/pages/speaking.js
@@ -27,9 +27,9 @@ const Speaking = ({ data }) => {
         ]}
       />
 
-      <div>
-        <h3>2022</h3>
-        <p
+    <h3>2022</h3>
+      <ul>
+        <li
           style={{
             marginBottom: 0,
           }}
@@ -41,8 +41,8 @@ const Speaking = ({ data }) => {
           >
             Post AWS re:Invent recap with #reInventHunt
           </a>
-        </p>
-        <p
+        </li>
+        <li
           style={{
             marginBottom: 0,
           }}
@@ -50,8 +50,8 @@ const Speaking = ({ data }) => {
           <a href="https://youtu.be/_kY7o3De_9A?t=15557" target="_blank" rel="noopener noreferrer">
             What I learned going from AWS SAM to CDK
           </a>
-        </p>
-        <p
+        </li>
+        <li
           style={{
             marginBottom: 0,
           }}
@@ -59,8 +59,8 @@ const Speaking = ({ data }) => {
           <a href="https://youtu.be/Ey7bNVT4W1g?t=10181" target="_blank" rel="noopener noreferrer">
             The local cloud, ideas to ensure developer productivity in serverless architectures
           </a>
-        </p>
-        <p
+        </li>
+        <li
           style={{
             marginBottom: 0,
           }}
@@ -68,12 +68,12 @@ const Speaking = ({ data }) => {
           <a href="https://www.youtube.com/watch?v=7QP5jdXb8WA" target="_blank" rel="noopener noreferrer">
             Serverless Architecture, an Honest Conversation
           </a>
-        </p>
-      </div>
+        </li>
+      </ul>
 
-      <div>
-        <h3>2020</h3>
-        <p
+    <h3>2020</h3>
+      <ul>
+        <li
           style={{
             marginBottom: 0,
           }}
@@ -81,17 +81,17 @@ const Speaking = ({ data }) => {
           <a href="https://youtu.be/8hmg4cgBEsg?t=8586" target="_blank" rel="noopener noreferrer">
             Serverless Mythbusters
           </a>
-        </p>
-        <p>
+        </li>
+        <li>
           <a href="https://www.youtube.com/watch?v=eW2rK4bnk1k" target="_blank" rel="noopener noreferrer">
             The Serverless Mindset
           </a>
-        </p>
-      </div>
+        </li>
+      </ul>
 
-      <div>
-        <h3>2019</h3>
-        <p
+    <h3>2019</h3>
+      <ul>
+        <li
           style={{
             marginBottom: 0,
           }}
@@ -99,8 +99,8 @@ const Speaking = ({ data }) => {
           <a href="https://www.pscp.tv/w/1yoJMzjeXypxQ" target="_blank" rel="noopener noreferrer">
             The Power of Serverless for Transforming Careers and Communities
           </a>
-        </p>
-        <p
+        </li>
+        <li
           style={{
             marginBottom: 0,
           }}
@@ -108,8 +108,8 @@ const Speaking = ({ data }) => {
           <a href="https://www.serverlesschats.com/25/" target="_blank" rel="noopener noreferrer">
             Serverless Chats Ep. 25
           </a>
-        </p>
-        <p>
+        </li>
+        <li>
           <a
             href="https://acloud.guru/series/serverlessconf-nyc-2019/view/leveling-up-in-serverless"
             target="_blank"
@@ -117,17 +117,17 @@ const Speaking = ({ data }) => {
           >
             Leveling Up in Serverless
           </a>
-        </p>
-      </div>
+        </li>
+      </ul>
 
-      <div>
-        <h3>2018</h3>
-        <p>
+    <h3>2018</h3>
+      <ul>
+        <li>
           <a href="https://www.youtube.com/watch?v=77rk1uYzayM" target="_blank" rel="noopener noreferrer">
             Yinzer's First Alexa Skill
           </a>
-        </p>
-      </div>
+        </li>
+      </ul>
     </Layout>
   );
 };
